cb45f3b50418d9ae28c40d2d06e7ae760a6cde2d
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-07-03 12:33:02 +0100

    Linting issues

--- tests/roles/experiment.spec.ts
+++ tests/roles/experiment.spec.ts
@@ -1,19 +1,8 @@
-import { jobRoles } from '../../data/jobRoles';
+import { makeApiCall } from '../../apiRequest/apiRequest';
 import { getMaximumId } from '../../helpers/helpers';
-import config from '../../playwright.config';
 import { createRole } from '../../queries/roleQueries';
 
-import { makeApiCall } from '../../apiRequest/apiRequest';
-
 const { test, expect } = require('@playwright/test');
-const endpoint = config.use?.baseURL ?? '';
-
-// this is used in all the queries
-const options = {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
 
 test('should allow a role to be created (Experimental Method)', async ({}) => {
   const roleName = 'superman';
@@ -30,7 +19,7 @@ test('should allow a role to be created (Experimental Method)', async ({}) => {
   };
 
   // call our new makiApiCall function and get the reply
-  let apiResponse = await makeApiCall(body);
+  const apiResponse = await makeApiCall(body);
 
   expect.soft(apiResponse.actualStatus, `Verify status code is 200`).toBe(200);
   expect.soft(apiResponse.actualRespose.data.RoleCreateOne.createdAt, `Verify createdAt contains today's date`).toContain(today);
